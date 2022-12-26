20f0e047f63b8ae080fce1b38adef6d58a7a26c4
vampirepapi <shubhamsourabh8@gmail.com>
2022-12-26 10:19:18 +0530

    list done

---
+++ DS-With-Python/List Basic/Left Rotate a List by one.py
@@ -0,0 +1,12 @@
+def rotateArr(l):
+    n = len(l)
+    x = l[0]
+
+    for i in range(1,n):
+        l[i-1] = l[i]
+    
+    l[n-1] = x
+    return l
+
+arr = [10,20,20,30,30,30,30,50]
+print(rotateArr(arr))
